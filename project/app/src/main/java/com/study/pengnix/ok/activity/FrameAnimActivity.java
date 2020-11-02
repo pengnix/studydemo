8f492efb6b83ea0e6c709d7257b9b1cb488c8b9f
pengnix <superpengnix@gmail.com>
2020-11-02 16:14:21 +0800

    CircularReveral anim demo

--- project/app/src/main/java/com/study/pengnix/ok/activity/FrameAnimActivity.java
+++ project/app/src/main/java/com/study/pengnix/ok/activity/FrameAnimActivity.java
@@ -1,11 +1,13 @@
 package com.study.pengnix.ok.activity;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.graphics.Path;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 
@@ -17,15 +19,23 @@ import com.study.pengnix.ok.R;
 public class FrameAnimActivity extends AppCompatActivity {
 
     private static String TAG = "Code";
+    ImageView oval;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_anim_layout_demo);
+        oval = (ImageView) findViewById(R.id.oval);
     }
     public void startAnim(View view) {
         ImageView image = (ImageView) findViewById(R.id.path_line);
         image.setImageResource(R.drawable.animation_list);
         AnimationDrawable anim = (AnimationDrawable) image.getDrawable();
         anim.start();
+    }
+
+    public void startAnim2(View view) {
+        Animator animator1 = ViewAnimationUtils.createCircularReveal(oval, oval.getWidth() / 2, oval.getHeight() / 2, oval.getWidth(), 0);
+        animator1.setDuration(2000);
+        animator1.start();
     }
 }
