e1b34610507e6e2737425d0202bf878d7e9aa86a
pengnix <superpengnix@gmail.com>
2020-11-18 16:33:47 +0800

    searchbox矢量图动画

--- project/app/src/main/java/com/study/pengnix/ok/activity/AnimVectorActivity.java
+++ project/app/src/main/java/com/study/pengnix/ok/activity/AnimVectorActivity.java
@@ -18,14 +18,16 @@ import com.study.pengnix.ok.R;
 public class AnimVectorActivity extends AppCompatActivity {
 
     private static String TAG = "Code";
-    ImageView imgBtn,iv2;
+    ImageView imgBtn,iv2,iv1;
     private boolean isTwitterChecked = false;
+    private boolean isSearchBoxChecked = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vector_layout_demo);
         imgBtn = findViewById(R.id.imgBtn);
         iv2 = findViewById(R.id.iv_2);
+        iv1 = findViewById(R.id.iv_1);
     }
 
     public void startAnim(View view){
@@ -37,5 +39,11 @@ public class AnimVectorActivity extends AppCompatActivity {
         isTwitterChecked = !isTwitterChecked;
         final int[] stateSet = {android.R.attr.state_checked * (isTwitterChecked ? 1 : -1)};
         iv2.setImageState(stateSet, true);
+    }
+
+    public void onSearchBoxClick(View view) {
+        isSearchBoxChecked = !isSearchBoxChecked;
+        final int[] stateSet = {android.R.attr.state_checked * (isSearchBoxChecked ? 1 : -1)};
+        iv1.setImageState(stateSet, true);
     }
 }
