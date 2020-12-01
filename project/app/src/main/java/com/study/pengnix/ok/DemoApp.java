bedfce13ea602a1d74dab0698e86767332530c50
pengnix <superpengnix@gmail.com>
2020-12-01 13:48:08 +0800

    1支持模拟器2支持StrictMode严格模式

--- project/app/src/main/java/com/study/pengnix/ok/DemoApp.java
+++ project/app/src/main/java/com/study/pengnix/ok/DemoApp.java
@@ -1,18 +1,46 @@
 package com.study.pengnix.ok;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import io.microshow.rxffmpeg.RxFFmpegInvoke;
 
 public class DemoApp extends Application {
 
+    boolean DEVELOPER_MODE = false;
+
     @Override
     public void onCreate() {
+        initStrictMode();
         super.onCreate();
-        initSDKs();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                initSDKs();
+            }
+        }).start();
     }
 
-    void initSDKs(){
+    void initSDKs() {
         RxFFmpegInvoke.getInstance().setDebug(true);
+    }
+
+    void initStrictMode() {
+        if (DEVELOPER_MODE) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//                    .detectDiskReads()
+//                    .detectDiskWrites()
+//                    .detectNetwork()
+                    .detectAll()// or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .penaltyDialog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
     }
 }
